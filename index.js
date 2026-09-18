bf1b2fe71f0ec9e1994e9a78b46133c04af5e9dc
nagodepranjal37-ops <nagodepranjal37@gmail.com>
2026-09-18 07:36:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>2.0.0 Lived - Node js application <br> using nexus </h1>')
+  res.send('<h1>welcome codingwale </h1>')
 })
 
 app.listen(port,'0.0.0.0', () => {
